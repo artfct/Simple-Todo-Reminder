1802868020896b48e67f779404dca0392f715e30
artfct <dae.jung@mail.utoronto.ca>
2021-06-11 16:24:12 -0400

    Double click to set reminder is active on App and passed proped to Tasks and Task

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,9 +1,16 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className='task'>
-            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} /> </h3>
+        <div className={`task ${task.reminder ?
+            'reminder h3, reminder p' : ''}`}
+            onClick={() => onToggle(task.id)}>
+            <h3>
+                {task.text}
+                <FaTimes
+                    style={{ color: 'red', cursor: 'pointer' }}
+                    onClick={() => onDelete(task.id)} />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
