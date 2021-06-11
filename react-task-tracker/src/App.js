616e5b2e82f377aab933aba209973fd9fc3fc460
artfct <dae.jung@mail.utoronto.ca>
2021-06-11 18:11:24 -0400

    Fetch existing tasks from db.json and update to tasks array for diaplay

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,32 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Doctor appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Google contract meeting',
-        day: 'Feb 6th at 3:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'UFable brand meeting',
-        day: 'Feb 7th at 5:30pm',
-        reminder: false,
-      },
-    ]
-  )
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   // Add Task
   const addTask = (task) => {
@@ -51,8 +48,10 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : ('No Tasks to show'
