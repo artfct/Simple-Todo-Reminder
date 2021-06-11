ed612d283852a289e63f830372822ab100a634a7
artfct <dae.jung@mail.utoronto.ca>
2021-06-11 19:21:19 -0400

    Connected local db json server. Add, Delete and Updates tasks and reminder status

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -25,25 +25,59 @@ function App() {
     return data
   }
 
+  // Fetch a Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000)
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    // json automatically assigns unique id
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => (
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask),
+      })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) =>
       task.id === id ? (
-        { ...task, reminder: !task.reminder }
+        { ...task, reminder: data.reminder }
       ) : task
-    )))
+    ))
   }
 
   return (
